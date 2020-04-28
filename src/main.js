474efcf7115a9e086392fadcbb83eeef4b8db57b
Alejandro Sanchez <rasso92@gmail.com>
2020-04-27 21:34:55 -0600

    Add bootstrap to the code base

--- src/main.js
+++ src/main.js
@@ -1,7 +1,13 @@
 import Vue from "vue";
 import App from "./App.vue";
-import router from "./router";
 import store from "./store";
+import router from "./router";
+import { BootstrapVue, IconsPlugin } from "bootstrap-vue";
+
+// Install BootstrapVue
+Vue.use(BootstrapVue);
+// Optionally install the BootstrapVue icon components plugin
+Vue.use(IconsPlugin);
 
 Vue.config.productionTip = false;
 
