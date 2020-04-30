2b02a431beef629e1bf71333f558db14dfdb1026
Alejandro Sanchez <rasso92@gmail.com>
2020-04-30 16:21:11 -0600

    Add the component name to the App components

--- src/App.vue
+++ src/App.vue
@@ -3,5 +3,14 @@
 		<router-view />
 	</div>
 </template>
-
+<script>
+export default {
+	/**
+	 * The component's name used for debugging.
+	 *
+	 * @type {String}
+	 */
+	name: 'App',
+}
+</script>
 <style lang="scss" src="./sass/app.scss"></style>
