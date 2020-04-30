fdb5a3282854616f9d96337c1a1d2ccf4658d1ab
Alejandro Sanchez <rasso92@gmail.com>
2020-04-30 16:23:47 -0600

    Add the login view to the base project

---
+++ src/views/Login.vue
@@ -0,0 +1,15 @@
+<template>
+	<div class="auth">
+		<div class="auth-left"></div>
+		<div class="auth-right">
+			<b-icon-star-half class="h1 my-5" />
+			<button class="btn-facebook">Sign in with Facebook</button>
+		</div>
+	</div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style></style>
