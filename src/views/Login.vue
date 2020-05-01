e72bf9b1aae8a71d0bb1fe44f57f6ec08ef2d913
Alejandro Sanchez <rasso92@gmail.com>
2020-04-30 18:21:20 -0600

    Refactor the login view to use the signIn component

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -3,24 +3,30 @@
 		<div class="auth-left"></div>
 		<div class="auth-right">
 			<b-icon-star-half class="h1 my-5" />
-			<button v-if="!authenticated" @click="login">
-				Sign in with Google
-			</button>
+			<SingIn />
 		</div>
 	</div>
 </template>
 
 <script>
-import Firebase from '../app/Firebase.js'
+import SingIn from '../components/SignIn.vue'
 
 export default {
+	/**
+	 * The component's registered child components.
+	 *
+	 * @type {Object}
+	 */
+	components: {
+		SingIn,
+	},
+
 	/**
 	 * The component's name used for debugging.
 	 *
 	 * @type {String}
 	 */
 	name: 'Login',
-
 }
 </script>
 
