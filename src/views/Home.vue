afee6b1278018e4f502718b2c8d088dff61d879e
Alejandro Sanchez <rasso92@gmail.com>
2020-04-30 18:21:45 -0600

    Add the home view to the code base

---
+++ src/views/Home.vue
@@ -0,0 +1,25 @@
+<template>
+	<Logout />
+</template>
+
+<script>
+import Logout from '../components/Logout.vue'
+
+export default {
+	/**
+	 * The component's registered child components.
+	 *
+	 * @type {Object}
+	 */
+	components: {
+		Logout,
+	},
+
+	/**
+	 * The component's name used for debugging.
+	 *
+	 * @type {String}
+	 */
+	name: 'Home',
+}
+</script>
