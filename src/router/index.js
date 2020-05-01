951b0e942f25ca55a39e3f67d1724598c603bb7c
Alejandro Sanchez <rasso92@gmail.com>
2020-04-30 18:20:52 -0600

    Update router to add the home view page

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,6 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
+import Home from '../views/Home.vue'
 import Login from '../views/Login.vue'
 
 Vue.use(VueRouter)
@@ -7,8 +8,13 @@ Vue.use(VueRouter)
 const routes = [
 	{
 		path: '/',
-		name: 'Login',
+		name: 'login',
 		component: Login,
+	},
+	{
+		path: '/home',
+		name: 'home',
+		component: Home,
 	},
 ]
 
