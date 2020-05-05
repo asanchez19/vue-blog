585df8ad2a2a8c18d80cc8d5d6b71fe07ebe75f9
Alejandro Sanchez <rasso92@gmail.com>
2020-05-05 10:32:15 -0600

    Add login view

---
+++ src/views/Login.vue
@@ -0,0 +1,61 @@
+<template>
+	<div class="auth">
+		<div class="auth-left"></div>
+		<div class="auth-right">
+			<b-icon-star-half class="h1 my-5" />
+			<button @click="login">Sign in with Google</button>
+		</div>
+	</div>
+</template>
+
+<script>
+import Firebase from '../app/Firebase'
+
+export default {
+	/**
+	 * The component's local methods.
+	 *
+	 * @type {Object}
+	 */
+	methods: {
+		async login() {
+			await Firebase.login()
+
+			if (this.user.loggedIn) {
+				this.$router.push({ name: 'Home' })
+			}
+		},
+	},
+
+	/**
+	 * The component's mounted lifecycle hook.
+	 *
+	 * @return {void}
+	 */
+	mounted() {
+		Firebase.auth.onAuthStateChanged(user => {
+			if (!user) {
+				this.user.loggedIn = false
+				return (this.user.data = {})
+			}
+
+			this.user.loggedIn = true
+			this.user.data = user
+		})
+	},
+
+	/**
+	 * Get the component's initial state.
+	 *
+	 * @return {Object}
+	 */
+	data() {
+		return {
+			user: {
+				loggedIn: false,
+				data: {},
+			},
+		}
+	},
+}
+</script>
