7acd8a35ac66814c1024be65b9b07abf63918c64
Alejandro Sanchez <rasso92@gmail.com>
2020-05-05 10:32:06 -0600

    Add home view

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,11 +1,63 @@
 <template>
-	<div class="home">
-		<img alt="Vue logo" src="../assets/logo.png" />
+	<div>
+		Hola {{ user.data.displayName }}
+		<button @click="logout">Logout</button>
 	</div>
 </template>
 
 <script>
+import Firebase from '../app/Firebase'
+
 export default {
-	name: 'Home',
+	/**
+	 * The component's local methods.
+	 *
+	 * @type {Object}
+	 */
+	methods: {
+		/**
+		 * Handle the logout event click.
+		 *
+		 * @return {void}
+		 */
+		async logout() {
+			await Firebase.logout()
+
+			if (!this.user.loggedIn) {
+				this.$router.push({ name: 'Login' })
+			}
+		},
+	},
+
+	/**
+	 * The component's mounted lifecycle hook.
+	 *
+	 * @return {void}
+	 */
+	mounted() {
+		Firebase.auth.onAuthStateChanged(user => {
+			if (!user) {
+				this.user.loggedIn = false
+				return (this.user.data = {})
+			}
+
+			this.user.loggedIn = true
+			this.user.data = user
+		})
+	},
+
+	/**
+	 * Get the component's initial state.
+	 *
+	 * @return {Object}
+	 */
+	data() {
+		return {
+			user: {
+				loggedIn: false,
+				data: {},
+			},
+		}
+	},
 }
 </script>
