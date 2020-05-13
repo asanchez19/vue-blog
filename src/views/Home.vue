75196e9f01930a5df71bc2669e449cf2861722a6
Alejandro Sanchez <rasso92@gmail.com>
2020-05-12 20:08:48 -0600

    Refactor home view component to use the new components

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,11 +1,46 @@
 <template>
 	<div>
-		<Logout />
+		<div class="container-fluid">
+			<div class="row content">
+				<Sidebar />
+
+				<div class="col-sm-9">
+					<h4><small>MIS POSTS</small></h4>
+					<hr />
+					<h2>I Love Food</h2>
+					<h5>
+						<span class="glyphicon glyphicon-time"></span> Post by
+						Jane Dane, Sep 27, 2015.
+					</h5>
+					<h5>
+						<span class="label label-danger">Food</span>
+						<span class="label label-primary">Ipsum</span>
+					</h5>
+					<br />
+					<p>
+						Food is my passion. Lorem ipsum dolor sit amet,
+						consectetur adipiscing elit, sed do eiusmod tempor
+						incididunt ut labore et dolore magna aliqua. Ut enim ad
+						minim veniam, quis nostrud exercitation ullamco laboris
+						nisi ut aliquip ex ea commodo consequat. Excepteur sint
+						occaecat cupidatat non proident, sunt in culpa qui
+						officia deserunt mollit anim id est laborum consectetur
+						adipiscing elit, sed do eiusmod tempor incididunt ut
+						labore et dolore magna aliqua. Ut enim ad minim veniam,
+						quis nostrud exercitation ullamco laboris nisi ut
+						aliquip ex ea commodo consequat.
+					</p>
+				</div>
+			</div>
+		</div>
+		<Footer />
 	</div>
 </template>
 
 <script>
-import Logout from '../components/Logout.vue'
+import Footer from '../components/Footer.vue'
+import Sidebar from '../components/Sidebar.vue'
+
 export default {
 	/**
 	 * The component's registered child components.
@@ -13,7 +48,8 @@ export default {
 	 * @type {Object}
 	 */
 	components: {
-		Logout,
+		Footer,
+		Sidebar,
 	},
 	/**
 	 * The component's name used for debugging.
