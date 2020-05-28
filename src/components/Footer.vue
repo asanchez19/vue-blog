3ce02a836695e197756435143da25efb917f0b08
Alejandro Sanchez <rasso92@gmail.com>
2020-05-28 14:00:11 -0600

    Handle props in the footer component

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -4,6 +4,7 @@
 			Copyright 2020 by
 			<a href="https://github.com/asanchez19"> Alejandro Sanchez </a> All
 			Rights Reserved.
+			<a @click="onVersionClick"> Version: {{ version }} </a>
 		</p>
 	</footer>
 </template>
@@ -11,11 +12,47 @@
 <script>
 export default {
 	/**
+	 * The component's local methods.
+	 *
+	 * @type {Object}
+	 */
+	methods: {
+		/**
+		 * Handle the click event.
+		 *
+		 * @return {void}
+		 */
+		onVersionClick() {
+			this.$emit(
+				'mi-emit',
+				'Este es un mensaje desde el componente footer.'
+			)
+		},
+	},
+
+	/**
 	 * The component's name used for debugging.
 	 *
 	 * @type {String}
 	 */
 	name: 'Footer',
+
+	/**
+	 * The component's inherited properties.
+	 *
+	 * @type {Object}
+	 */
+	props: {
+		/**
+		 * The app version.
+		 *
+		 * @type {String}
+		 */
+		version: {
+			type: String,
+			default: '1.0',
+		},
+	},
 }
 </script>
 
